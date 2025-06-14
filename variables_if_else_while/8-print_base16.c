e1b686dfc474c01c734acea3f55ab501ca9a301c
zgurbanva <zeyneb.gurbanva@gmail.com>
2025-06-14 16:49:04 +0000

    commit

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        if (i < 10)
-            putchar(i + '0');      /* Print digits 0 to 9 */
-        else
-            putchar(i - 10 + 'a'); /* Print letters a to f */
-    }
-    putchar('\n');                  /* Print new line */
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+			putchar(i + '0');      /* Print digits 0 to 9 */
+		else
+			putchar(i - 10 + 'a'); /* Print letters a to f */
+	}
+	putchar('\n');                  /* Print new line */
 
-    return (0);
+	return (0);
 }
