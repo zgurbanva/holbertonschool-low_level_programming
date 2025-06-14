c76610589293acf14d1797a83b0455058a99766b
zgurbanva <zeyneb.gurbanva@gmail.com>
2025-06-14 15:32:20 +0000

    Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
+	/**
+ * main - Prints a message using puts
+ *
+ * Return: Always 0 (Success)
+ */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
