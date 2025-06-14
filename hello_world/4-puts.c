ec965fcdcda07929018a524cae352997ac9e976e
zgurbanva <zeyneb.gurbanva@gmail.com>
2025-06-14 15:43:05 +0000

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(void)
-{
-	/**
- * main - Prints a message using puts
+/**
+ * main - Prints a string using puts
  *
  * Return: Always 0 (Success)
  */
-
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
 }
