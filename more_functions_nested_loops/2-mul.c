fa7d48f295b9a20dddd4b0c8dc3944a506e8c6ce
Zeynab <zeyneb.gurbanva@gmail.com>
2025-06-18 11:47:02 +0000

    commit

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of multiplication
+ */
 int mul(int a, int b)
-	
-	
 {
-	return (a*b);
+	return (a * b);
 }
