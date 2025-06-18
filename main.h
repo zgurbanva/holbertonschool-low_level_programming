3030cd77c71696b9c8bd1ea1041ccadf46ea83a4
Zeynab <zeyneb.gurbanva@gmail.com>
2025-06-18 10:47:15 +0000

    commit

---
+++ main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_M
+#define MAIN_N
+int _isupper(int c);
+#endif
---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c){
+        if (c>='0' && c<='9')
+                return 1;
+        else
+                return 0;
+}
