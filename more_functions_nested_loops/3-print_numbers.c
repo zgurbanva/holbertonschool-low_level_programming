f21590d0198000e10b0481483846044dd5871c04
Zeynab <zeynab.gurbanva@gmail.com>
2025-06-21 09:10:58 +0000

    commit

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,15 @@
 /**
  * print_numbers - Prints the numbers from 0 to 9 followed by a new line.
  *
- * Description: Uses only _putchar twice to print numbers and a newline.
+ * Description: Uses only _putchar twice to print the numbers and newline.
  */
 void print_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        _putchar('0' + i);
-    }
-    _putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
 }
