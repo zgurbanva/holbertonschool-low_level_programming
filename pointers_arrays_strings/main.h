bd4ba65ecad13e8eb7bf7c0002ddb36932ee8894
Zeynab <zeynab.gurbanva@gmail.com>
2025-06-21 13:27:49 +0000

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,6 +16,7 @@ int _atoi(char *s);
 void puts2(char *str);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
 
