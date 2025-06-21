1564fa0aefea4b0f1253ba5b8f7ec3ee5e186366
Zeynab <zeynab.gurbanva@gmail.com>
2025-06-21 13:29:12 +0000

    commit

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 
+ *   0 if s1 and s2 are equal,
+ *   positive value if s1 > s2,
+ *   negative value if s1 < s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,6 +17,7 @@ void puts2(char *str);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
 
