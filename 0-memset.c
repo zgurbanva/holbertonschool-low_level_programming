1fcc7ef1f0eafcc9ec69b14b35047261a8cf1a02
Zeynab <zeyneb.gurbanva@gmail.com>
2025-06-24 08:16:01 +0000

    commit

--- 0-memset.c
+++ 0-memset.c
@@ -8,12 +8,14 @@
  *
  * Return: pointer to the memory area s
  */
-char *_memset(char *s, char b, unsigned int n)
+char	*_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
- {
-	 s[i] = b;
- }
+	{
+		s[i] = b;
+	}
+
 	return (s);
 }
