04e8a0cad1ecc545ae95e37648107dc6464a7b39
Zeynab <zeyneb.gurbanva@gmail.com>
2025-07-05 08:56:09 +0000

    commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure to store dog information
+ * @name: Name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (string)
+ *
+ * Description: Defines a new type struct dog with name, age, and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
