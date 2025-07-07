3993fb2df3749b13e0bd3db2984e65e358f0ed74
Zeynab <zeyneb.gurbanva@gmail.com>
2025-07-07 13:51:23 +0000

    commit

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * print_name - Prints a name using a callback function
+ * @name: The name to print
+ * @f: Pointer to a function that takes a char * and returns void
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
---
+++ function_pointers/main.h
@@ -0,0 +1,4 @@
+#ifdef MAIN_H
+#define MAIN_H
+void print_name(char *name, void (*f)(char *));
+#endif
