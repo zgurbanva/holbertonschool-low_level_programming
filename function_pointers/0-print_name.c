4f6c56a15a8506983b7a8edd7d361adcaeeece94
Zeynab <zeyneb.gurbanva@gmail.com>
2025-07-12 09:46:24 +0000

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,6 +6,8 @@
  * @name: The name to print
  * @f: Pointer to a function that takes a char * and returns void
  *
+ * Description: If both name and function pointer are not NULL,
+ *              calls the function pointed to by f with name as argument.
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
