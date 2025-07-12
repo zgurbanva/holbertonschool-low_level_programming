2df8f86c41d150aefa4245ddbbb1dac7f6dba3d9
Zeynab <zeyneb.gurbanva@gmail.com>
2025-07-12 10:14:02 +0000

    commit

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -4,7 +4,7 @@
  * op_add - Returns the sum of two integers.
  * @a: First integer
  * @b: Second integer
- * 
+ *
  * Return: The sum of a and b.
  */
 int op_add(int a, int b)
@@ -16,7 +16,7 @@ int op_add(int a, int b)
  * op_sub - Returns the difference of two integers.
  * @a: First integer
  * @b: Second integer
- * 
+ *
  * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
@@ -28,7 +28,7 @@ int op_sub(int a, int b)
  * op_mul - Returns the product of two integers.
  * @a: First integer
  * @b: Second integer
- * 
+ *
  * Return: The product of a and b.
  */
 int op_mul(int a, int b)
@@ -40,7 +40,7 @@ int op_mul(int a, int b)
  * op_div - Returns the quotient of two integers.
  * @a: First integer (dividend)
  * @b: Second integer (divisor)
- * 
+ *
  * Return: The quotient of a divided by b.
  */
 int op_div(int a, int b)
@@ -52,7 +52,7 @@ int op_div(int a, int b)
  * op_mod - Returns the remainder of the division of two integers.
  * @a: First integer (dividend)
  * @b: Second integer (divisor)
- * 
+ *
  * Return: The remainder of a divided by b.
  */
 int op_mod(int a, int b)
