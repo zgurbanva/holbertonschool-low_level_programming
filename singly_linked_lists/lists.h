37795a035819ada634a228ec360342c0f2e5116a
Zeynab <zeyneb.gurbamva@gmail.com>
2025-07-22 09:42:42 +0000

    commit

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -20,5 +20,5 @@ size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-
+size_t print_list(const list_t *h);
 #endif
