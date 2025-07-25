c6f65e5e427378d9779d9007b5783b8d341061d8
Zeynab <zeyneb.gurbanva@gmail.com>
2025-07-25 06:06:06 +0000

    commit

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - Retrieves the value associated with a key
+ * @ht: The hash table to look into
+ * @key: The key to search for
+ *
+ * Return: The value associated with the key, or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
