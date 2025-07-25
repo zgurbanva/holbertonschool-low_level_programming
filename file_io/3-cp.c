e470e0f4d2969830be4868ba0ad76098335d2d5f
Zeynab <zeyneb.gurbanva@gmail.com>
2025-07-25 06:19:41 +0000

    commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,36 +1,37 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- * print_error - Prints an error message and exits with given code
- * @exit_code: Code to exit with
- * @message: Error message format string
- * @arg: Argument to include in error message
+ * print_error - Print error message and exit with given code.
+ * @exit_code: The exit code.
+ * @format: Error message format string.
+ * @arg: Argument for the format string (usually a filename or fd).
  */
-void print_error(int exit_code, const char *message, const char *arg)
+void print_error(int exit_code, const char *format, const char *arg)
 {
-	dprintf(STDERR_FILENO, message, arg);
+	dprintf(STDERR_FILENO, format, arg);
 	exit(exit_code);
 }
 
 /**
- * main - Copies the content of a file to another file
- * @argc: Argument count
- * @argv: Argument vector
- * Return: 0 on success, exits with code on error
+ * main - Copies the contents of one file to another.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: 0 on success, exits with specific code on failure.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w, c;
+	int fd_from, fd_to, r, w;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		print_error(97, "Usage: cp file_from file_to\n", "");
+		print_error(97, "Usage: cp %s %s\n", "file_from file_to");
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
@@ -53,6 +54,7 @@ int main(int argc, char *argv[])
 			print_error(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
+
 	if (r == -1)
 	{
 		close(fd_from);
@@ -60,12 +62,10 @@ int main(int argc, char *argv[])
 		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 
-	c = close(fd_from);
-	if (c == -1)
+	if (close(fd_from) == -1)
 		print_error(100, "Error: Can't close fd %d\n", argv[1]);
 
-	c = close(fd_to);
-	if (c == -1)
+	if (close(fd_to) == -1)
 		print_error(100, "Error: Can't close fd %d\n", argv[2]);
 
 	return (0);
